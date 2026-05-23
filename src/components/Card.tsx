305818e9e559c803a6665aaeb3456cbee9d10605
Matías Méndez <matiasmendez722@gmail.com>
2026-05-23 20:15:53 -0300

    .

---
+++ src/components/Card.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styles from "../styles/components/Card.module.css";
+
+interface CardProps {
+  title: string;
+  shortText: string;
+  longText: string;
+}
+
+export function Card({ title, shortText, longText }: CardProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleReadMore = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <div className={styles.card}>
+      <h3 className={styles.title}>{title}</h3>
+      <p>
+        {shortText}
+        {isExpanded && (
+          <div className="long-text-container">
+            <p>{longText}</p>
+          </div>
+        )}
+      </p>
+
+      <button onClick={toggleReadMore} className={styles.button}>
+        {isExpanded ? "Leer menos" : "Leer más"}
+      </button>
+    </div>
+  );
+}
