a02113be0527359afbd86a105ca186d63e15fb2b
milena-martinez04 <mariamilenamartinez04@gmail.com>
2026-05-29 14:04:42 -0300

    Arreglo bug del footer

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../components/NavBar";
 import { Footer } from "../components/Footer";
-import styles from "@/styles/layouts/Layout.module.css";
+import styles from "../styles/layouts/Layout.module.css";
 
 export function Layout() {
   return (
--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer";
-import styles from "../layouts/Layout.module.css";
+import styles from "../styles/layouts/Layout.module.css";
 
 export function UserLayout() {
   return (
