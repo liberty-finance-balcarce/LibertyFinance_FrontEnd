0cecbdf7c8ff7a4fce1ff98c8b774cea93099c5e
Matías Méndez <matiasmendez722@gmail.com>
2026-06-08 22:12:12 -0300

    Implementacion del LoadingSpinner en el componente Ranking de Instrumentos Financieros

--- src/components/RankingInstrumentosFinancieros.tsx
+++ src/components/RankingInstrumentosFinancieros.tsx
@@ -8,7 +8,7 @@ export function RankingInstrumentosFinancieros() {
   if (isLoading) {
     return (
       <div className={styles.loadingContainer}>
-        <LoadingSpinner logoSrc="assets/logo-icon.png" size={120} />
+        <LoadingSpinner logo="assets/logo-icon.png" size={120} />
       </div>
     );
   }
