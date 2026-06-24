4bccbdcb8a51d05b6ef56fa195aa2d93c35835b7
Matías Méndez <matiasmendez722@gmail.com>
2026-06-24 00:28:14 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
         <Routes>
           <Route element={<UserRoute />}>
             <Route element={<UserLayout />}>
-              <Route path="/dashboard" element={<UserDashboard />}>
+              <Route path="/dashboard/user" element={<UserDashboard />}>
                 <Route path="cart" element={<DashboardCart />} />
                 <Route path="inversiones" element={<DashboardInversiones />} />
                 <Route path="test-inversor" element={<DashboardTestPerfil />} />
@@ -41,8 +41,9 @@ function App() {
 
           <Route element={<AdminRoute />}>
             <Route element={<AdminLayout />}>
-              <Route path="/dashboard" element={<AdminDashboard />} />
-              <Route path="users" element={<AdminCardUsers totalRegistrados={0} totalUsuarios={0} totalAdmins={0} />} />
+              <Route path="/dashboard/admin" element={<AdminDashboard />}>
+                <Route path="users" element={<AdminCardUsers totalRegistrados={0} totalUsuarios={0} totalAdmins={0} />} />
+              </Route>
             </Route>
           </Route>
 
