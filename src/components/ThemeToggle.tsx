a94abbcb48eb784d6ca90ba0102abebd6006836c
Matías Méndez <matiasmendez722@gmail.com>
2026-06-25 18:11:50 -0300

    Modo claro oscuro

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,33 +1,33 @@
-import styles from '../styles/components/ThemeToggle.module.css'
+import styles from "../styles/components/ThemeToggle.module.css";
 import { BsSunFill, BsMoonFill } from "react-icons/bs";
 
-import type { ThemeMode } from '../types/themeMode'
+import type { ThemeMode } from "../types/themeMode";
 
 interface Props {
-    theme: ThemeMode
-    onToggle: () => void
+  theme: ThemeMode;
+  onToggle: () => void;
 }
 
 export function ThemeToggle({ theme, onToggle }: Props) {
-    const isDark = theme === 'dark'
+  const isDark = theme === "dark";
 
-    return (
-     <button
-    className={styles.button}
-    onClick={onToggle}
-    aria-label={isDark ? 'Activar modo claro' : 'Activar modo oscuro'}
->
-    <BsSunFill
-        className={`${styles.icon} ${
-            isDark ? styles.active : styles.inactive
-        }`}
-    />
+  return (
+    <button
+      className={`${styles.button} ${
+        isDark ? styles.darkMode : styles.lightMode
+      }`}
+      onClick={onToggle}
+      aria-label={isDark ? "Activar modo claro" : "Activar modo oscuro"}
+    >
+      <BsSunFill
+        className={`${styles.icon} ${isDark ? styles.active : styles.inactive}`}
+      />
 
-    <BsMoonFill
+      <BsMoonFill
         className={`${styles.icon} ${
-            !isDark ? styles.active : styles.inactive
+          !isDark ? styles.active : styles.inactive
         }`}
-    />
-</button>
-    )
+      />
+    </button>
+  );
 }
