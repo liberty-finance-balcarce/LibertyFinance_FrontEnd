1d2f3b72c1a718d885fec932be0aeb22430b81c5
Matías Méndez <matiasmendez722@gmail.com>
2026-06-27 19:10:54 -0300

    .

--- src/components/AvatarUsuario.tsx
+++ src/components/AvatarUsuario.tsx
@@ -1,23 +1,22 @@
+import { useState } from "react";
 import styles from "../styles/components/NavBar.module.css";
 import type { Usuario } from "../types/usuarios";
 
 export function AvatarUsuario({ usuario }: { usuario: Usuario }) {
+  const [imageError, setImageError] = useState(false);
 
-    const iniciales =   `${usuario.nombre?.[0] ?? ""}${usuario.apellido?.[0] ?? ""}`.toUpperCase();
+  const iniciales =
+    `${usuario.nombre?.[0] ?? ""}${usuario.apellido?.[0] ?? ""}`.toUpperCase();
 
-    if(usuario.imagen) {
-        return (
-            <img
-            src={usuario.imagen}
-            alt="Imagen de perfil"
-            className={styles.usuario}
-            />
-        );
-        
-    }
-        return (
-            <div className={styles.usuario}>
-                {iniciales}
-            </div>
-        )
+  if (usuario.foto_perfil && !imageError) {
+    return (
+      <img
+        src={usuario.foto_perfil}
+        alt="Imagen de perfil"
+        className={styles.avatarImage}
+        onError={() => setImageError(true)}
+      />
+    );
+  }
+  return <div className={styles.avatarImage}>{iniciales}</div>;
 }
